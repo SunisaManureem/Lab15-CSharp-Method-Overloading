69d26fb86f84dd7ee800f494eb2bcdde6c8e8df7
SunisaManureem <sunisa2561a@gmail.com>
2025-03-15 19:39:52 +0700

    ส่งงาน

---
+++ Lab15_Ex07/Program.cs
@@ -0,0 +1,37 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Derived_2 d2 = new Derived_2();
+        Base b = (Base) d2;
+        Derived_1 d1 = (Derived_1) d2;
+
+        b.A();  // เรียกใช้เมธอดจาก Base
+        d1.A(); // เรียกใช้เมธอดจาก Derived_1
+        d2.A(); // เรียกใช้เมธอดจาก Derived_2
+    }
+}
+
+class Base
+{
+    public virtual void A()
+    {
+        System.Console.WriteLine("Base.A()");
+    }
+}
+
+class Derived_1 : Base
+{
+    public override void A()
+    {
+        System.Console.WriteLine("Derived_1.A()");
+    }
+}
+
+class Derived_2 : Derived_1
+{
+    public override void A()
+    {
+        System.Console.WriteLine("Derived_2.A()");
+    }
+}
